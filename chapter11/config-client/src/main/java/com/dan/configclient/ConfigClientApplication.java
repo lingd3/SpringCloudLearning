7e72f352a89ffa1022039b5a1ee1fcfb2c145ae2
lingd3 <824928207@qq.com>
2018-02-01 22:31:46 +0800

    高可用的分布式配置中心

---
+++ chapter11/config-client/src/main/java/com/dan/configclient/ConfigClientApplication.java
@@ -0,0 +1,14 @@
+package com.dan.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+}
---
+++ chapter11/config-server/src/main/java/com/dan/configserver/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.dan.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableDiscoveryClient
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
